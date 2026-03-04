7c58942793536daf1ed75e28bdee88e8958c8855
pari78721-eng <pari78721@gmail.com>
2026-03-04 20:59:09 +0530

    Add files via upload

---
+++ day032.c
@@ -0,0 +1,77 @@
+/*Problem: Implement push and pop operations on a stack and verify stack operations.
+
+Input:
+- First line: integer n
+- Second line: n integers to push
+- Third line: integer m (number of pops)
+
+Output:
+- Print remaining stack elements from top to bottom
+
+Example:
+Input:
+5
+10 20 30 40 50
+2
+
+Output:
+30 20 10*/
+#include <stdio.h>
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+// Push operation
+void push(int value)
+{
+    if (top == MAX - 1)
+    {
+        return; // Stack overflow (ignored as per problem)
+    }
+    top++;
+    stack[top] = value;
+}
+
+// Pop operation
+void pop()
+{
+    if (top == -1)
+    {
+        return; // Stack underflow (ignored as per problem)
+    }
+    top--;
+}
+
+int main()
+{
+    int n, m, i, value;
+
+    // Read number of elements to push
+    scanf("%d", &n);
+
+    // Read elements and push into stack
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &value);
+        push(value);
+    }
+
+    // Read number of pops
+    scanf("%d", &m);
+
+    // Perform pop operations
+    for (i = 0; i < m; i++)
+    {
+        pop();
+    }
+
+    // Print remaining stack from top to bottom
+    for (i = top; i >= 0; i--)
+    {
+        printf("%d ", stack[i]);
+    }
+
+    return 0;
+}
