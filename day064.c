63f6122c6e2c973dc3dac26978c88836156a5a6e
pari78721-eng <pari78721@gmail.com>
2026-04-05 22:00:05 +0530

    Add files via upload

---
+++ day064.c
@@ -0,0 +1,81 @@
+/*Problem: Perform BFS from a given source using queue.
+
+Input:
+- n
+- adjacency list
+- source s
+
+Output:
+- BFS traversal order*/
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int queue[MAX], front = -1, rear = -1;
+
+void enqueue(int x)
+{
+    if (rear == MAX - 1)
+        return;
+    if (front == -1)
+        front = 0;
+    queue[++rear] = x;
+}
+
+int dequeue()
+{
+    if (front == -1 || front > rear)
+        return -1;
+    return queue[front++];
+}
+
+int isEmpty()
+{
+    return (front == -1 || front > rear);
+}
+
+void BFS(int adj[MAX][MAX], int n, int s)
+{
+    int visited[MAX] = {0};
+
+    enqueue(s);
+    visited[s] = 1;
+
+    while (!isEmpty())
+    {
+        int node = dequeue();
+        printf("%d ", node);
+
+        for (int i = 0; i < n; i++)
+        {
+            if (adj[node][i] == 1 && !visited[i])
+            {
+                enqueue(i);
+                visited[i] = 1;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, s;
+    int adj[MAX][MAX];
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &adj[i][j]);
+
+    printf("Enter source node: ");
+    scanf("%d", &s);
+
+    printf("BFS Traversal: ");
+    BFS(adj, n, s);
+
+    return 0;
+}
